cf49cc40cf409669387967e3f5291f0c93e75fe9
Hiroyuki Yamaoka <yamaoka@10mado.jp>
2018-11-25 23:55:57 +0900

    Dispatcherの基本のテスト、用意

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -9,12 +9,22 @@ use Zend\Diactoros\ServerRequest;
 
 class DispatcherTest extends TestCase
 {
+    /**
+     * @runInSeparateProcess
+     */
     public function testAction()
     {
         $container = new FakeContainer();
         $dispatcher = new Dispatcher($container);
+
+        ob_start();
         $dispatcher->dispatch([
             new FakeAction(),
         ]);
+        $headers = headers_list();
+        $res = ob_get_clean();
+
+        $this->assertEquals(0, count($headers));
+        $this->assertEquals('', $res);
     }
 }
