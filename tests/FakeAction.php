f4d78f9afdda7eba2330e488bb27c4e4df0f4aeb
Hiroyuki Yamaoka <yamaoka@10mado.jp>
2018-11-26 11:56:09 +0900

    FakeActionで返すのをHtmlResponseに変更

--- tests/FakeAction.php
+++ tests/FakeAction.php
@@ -7,12 +7,12 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Zend\Diactoros\Response as ZendResponse;
+use Zend\Diactoros\Response\HtmlResponse;
 
 class FakeAction implements MiddlewareInterface
 {
     public function process(Request $request, RequestHandler $next): Response
     {
-        return new ZendResponse();
+        return new HtmlResponse('');
     }
 }
